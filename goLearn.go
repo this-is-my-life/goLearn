e2d95c1cae6d0aadc2e4274137b4ce9220935e2d
PMHStudio <pmhstudio.pmh@gmail.com>
2020-01-03 23:42:41 +0900

    astilectron

---
+++ goLearn.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/asticode/go-astilectron"
+)
+
+func main() {
+	app, err := astilectron.New(astilectron.Options{
+		AppName: "Go Learn - Astilectron",
+	})
+	erring(err)
+
+	defer app.Close()
+
+	err = app.Start()
+	erring(err)
+
+	w, err := app.NewWindow("src/index.html", &astilectron.WindowOptions{})
+	erring(err)
+
+	w.Create()
+
+	app.Wait()
+}
+
+func erring(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
